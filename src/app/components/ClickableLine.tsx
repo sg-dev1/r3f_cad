a12872eeed3adb7ce856d3c5052c9830df68c88f
Stefan Gschiel <stefan.gschiel.ext@siemens-energy.com>
2024-03-22 08:43:35 +0100

    Draw a line on mouse over; made ClickableLine its own component

---
+++ src/app/components/ClickableLine.tsx
@@ -0,0 +1,97 @@
+// TODOs
+// - snap to parallel for x/y axis
+// - tangent, parallel, ... contraints  --> needs a constraint solver   (e.g. use solvespace in a "backend")
+// TODO it would be also good to have a behaviour where the lines are closed, e.g. detect when a new point is close to and existing
+//      --> then use the existing one
+// TODO save common state in the background, e.g. points/ lines drawn --> what to use for state management
+//      react-redux? zustand (which comes from r3f)?
+//
+// TODO check if this implementation of line drawing (using a raycaster its setFromCamera function) is the best way to do it,
+//      e.g. it requires the creation of THREE.Vector2 instance all the time, are there alternative ways?
+// TODO The ClickableLine component will be the line drawing tool for the sketcher
+import React, { useState, forwardRef, useImperativeHandle, useEffect } from 'react';
+import { useThree } from '@react-three/fiber';
+import { Line } from '@react-three/drei';
+import { Vector3 } from 'three';
+import * as THREE from 'three';
+
+export interface ClickableLineRefType {
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onPointerOver: (event: React.PointerEvent<HTMLDivElement>) => void;
+  onPointerMove: (event: React.PointerEvent<HTMLDivElement>) => void;
+}
+
+const ClickableLine = forwardRef<any, any>(({}, ref) => {
+  const [points, setPoints] = useState<Vector3[]>([]);
+  const [currentMousePos, setCurrentMousePos] = useState<Vector3 | undefined>(undefined);
+  const [pointsToDraw, setPointsToDraw] = useState<Vector3[]>([]);
+  const { camera, scene, raycaster } = useThree();
+
+  const calcIntersection = (xCoord: number, yCoord: number, target: HTMLElement) => {
+    const rect = (target as HTMLElement).getBoundingClientRect();
+    const x = ((xCoord - rect.left) / rect.width) * 2 - 1;
+    const y = (-(yCoord - rect.top) / rect.height) * 2 + 1;
+    const point = new THREE.Vector2(x, y);
+
+    console.log('x=', y, 'y=', y, 'clientX=', xCoord, 'clientY=', yCoord);
+
+    raycaster.setFromCamera(point, camera);
+    // For this to work the scene must have children, e.g. adding the boxes
+    // Maybe the camera controls should be disabled --> weird behaviour
+    const [intersect] = raycaster.intersectObjects(scene.children, true);
+
+    return intersect;
+  };
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      onClick: (event: React.MouseEvent<HTMLElement>) => {
+        event.stopPropagation();
+        //console.log(event);
+
+        const intersect = calcIntersection(event.clientX, event.clientY, event.target as HTMLElement);
+        if (intersect) {
+          setPoints((points) => [...points, intersect.point]);
+        }
+        console.log(intersect);
+      },
+      onPointerOver: (event: React.PointerEvent<HTMLDivElement>) => {
+        event.stopPropagation();
+
+        console.log('onPointerOver', event);
+      },
+      onPointerMove: (event: React.PointerEvent<HTMLDivElement>) => {
+        event.stopPropagation();
+
+        //console.log('onPointerMove', event);
+        const intersect = calcIntersection(event.clientX, event.clientY, event.target as HTMLElement);
+        setCurrentMousePos(intersect?.point);
+      },
+    }),
+    [camera, scene, raycaster]
+  );
+
+  useEffect(() => {
+    if (currentMousePos) {
+      setPointsToDraw([...points, currentMousePos]);
+    } else {
+      setPointsToDraw(points);
+    }
+  }, [currentMousePos]);
+
+  return (
+    <>
+      {pointsToDraw.length > 1 && (
+        <Line
+          points={pointsToDraw} // Array of THREE.Vector3
+          color="pink"
+          lineWidth={1} // In pixels (default)
+          dashed={false} // Default
+        />
+      )}
+    </>
+  );
+});
+
+export default ClickableLine;
--- src/app/components/SketcherView.tsx
+++ src/app/components/SketcherView.tsx
@@ -3,21 +3,9 @@
 //      Plane should lie on the X and Y axes, e.g. looking from front on it with ortographic camera  (XY plane)  - in future all different planes shall be supported
 //      - current issue of solution is that plane is limited in size
 //      - app also behaves strangely together with orbit controls (maybe disable it - only support zooming and panning and no rotation)
-// TODO it would be nicer if line to be drawn is already displayed (based on last point clicked and current position of mouse)
-//      - here also a snap to parallel to x/y axis could be implemented
-// TODO it would be also good to have a behaviour where the lines are closed, e.g. detect when a new point is close to and existing
-//      --> then use the existing one
-// TODO save common state in the background, e.g. points/ lines drawn --> what to use for state management
-//      react-redux? zustand (which comes from r3f)?
 //
 // TODO improve the forward ref (what types to use instead of any)
 // TODO disable camera rotation (only zooming and paning should be allowed) - rotation creates weird behaviour
-// TODO check if this implementation of line drawing (using a raycaster its setFromCamera function) is the best way to do it,
-//      e.g. it requires the creation of THREE.Vector2 instance all the time, are there alternative ways?
-// TODO ClickableLine component should be moved into its own tsx file
-//
-// TODO The ClickableLine component will be the line drawing tool for the sketcher
-
 'use client';
 
 import React, { useRef, useState, forwardRef, useImperativeHandle } from 'react';
@@ -25,6 +13,7 @@ import { Canvas, useThree } from '@react-three/fiber';
 import { CameraControls, OrthographicCamera, Line, Plane } from '@react-three/drei';
 import { BoxGeometry, Vector3 } from 'three';
 import * as THREE from 'three';
+import ClickableLine, { ClickableLineRefType } from './ClickableLine';
 
 // This is just a test object and will be removed in the future
 const Box = (props: any) => {
@@ -38,61 +27,17 @@ const Box = (props: any) => {
   );
 };
 
-interface ClickableLineRefType {
-  onClick: (event: React.MouseEvent<HTMLElement>) => void;
-}
-
-const ClickableLine = forwardRef<any, any>(({}, ref) => {
-  const [points, setPoints] = useState<Vector3[]>([]);
-  const { camera, scene, raycaster } = useThree();
-
-  useImperativeHandle(
-    ref,
-    () => ({
-      onClick: (event: React.MouseEvent<HTMLElement>) => {
-        event.stopPropagation();
-
-        //console.log(event);
-
-        const rect = (event.target as HTMLElement).getBoundingClientRect();
-        const x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
-        const y = (-(event.clientY - rect.top) / rect.height) * 2 + 1;
-        const point = new THREE.Vector2(x, y);
-        console.log('x=', y, 'y=', y, 'clientX=', event.clientX, 'clientY=', event.clientY);
-        //const raycaster = new THREE.Raycaster();
-        raycaster.setFromCamera(point, camera);
-        // For this to work the scene must have children, e.g. adding the boxes
-        // Maybe the camera controls should be disabled --> weird behaviour
-        const [intersect] = raycaster.intersectObjects(scene.children, true);
-        if (intersect) {
-          setPoints((points) => [...points, intersect.point]);
-        }
-        console.log(intersect);
-      },
-    }),
-    [camera, scene, raycaster]
-  );
-
-  return (
-    <>
-      {points.length > 1 && (
-        <Line
-          points={points} // Array of THREE.Vector3
-          color="pink"
-          lineWidth={1} // In pixels (default)
-          dashed={false} // Default
-        />
-      )}
-    </>
-  );
-});
-
 const SketcherView = () => {
   const clickRef = React.useRef<ClickableLineRefType>(null);
 
   return (
     <>
-      <Canvas className="sketcherview" onClick={(e) => clickRef.current?.onClick(e)}>
+      <Canvas
+        className="sketcherview"
+        onClick={(e) => clickRef.current?.onClick(e)}
+        //onPointerOver={(e) => clickRef.current?.onPointerOver(e)}
+        onPointerMove={(e) => clickRef.current?.onPointerMove(e)}
+      >
         <CameraControls minDistance={1.2} maxDistance={4} />
 
         <ambientLight intensity={0.25} />
