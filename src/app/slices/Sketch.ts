759fd262273b513f81779e8c9c9531326c2c5f5e
Stefan G <sg-dev1@users.noreply.github.com>
2024-04-11 08:11:07 +0200

    Moved sketch specific code into its own module (Sketch.ts) and added the SketchType

---
+++ src/app/slices/Sketch.ts
@@ -0,0 +1,156 @@
+import { ConstraintType, SlvsConstraints } from '../types/Constraints';
+import { GeometryType, geometryTypeToString } from '../types/EntityType';
+import { Line3DType } from '../types/Line3DType';
+import { Point3DMapType, Point3DType } from '../types/Point3DType';
+import { SolverEntityType } from '../types/SolverTypes';
+
+export interface SketchType {
+  entityIdCounter: number;
+  points: Point3DType[];
+  pointsMap: Point3DMapType;
+  lines: Line3DType[];
+
+  // required for drawing of lines (stores the last point), not needed to persist
+  lastPoint3D: Point3DType | null;
+
+  constraintIdCounter: number;
+  constraints: ConstraintType[];
+}
+
+export const emptySketch: SketchType = {
+  entityIdCounter: 1,
+  points: [],
+  pointsMap: {},
+  lines: [],
+  lastPoint3D: null,
+
+  constraintIdCounter: 0,
+  constraints: [],
+};
+
+export const sketchAddEntity = (sketch: SketchType, p: Point3DType, type: GeometryType): SketchType => {
+  const newPoint = { ...p, id: sketch.entityIdCounter };
+  sketch.entityIdCounter++;
+
+  // TODO properly handle other types when they are supported
+  if (GeometryType.LINE === type) {
+    if (sketch.lastPoint3D) {
+      if (!(sketch.lastPoint3D.id in sketch.pointsMap)) {
+        sketch.points.push(sketch.lastPoint3D);
+        sketch.pointsMap[sketch.lastPoint3D.id] = sketch.lastPoint3D;
+      }
+      sketch.points.push(newPoint);
+      sketch.pointsMap[newPoint.id] = newPoint;
+      // add the line
+      sketch.lines.push({ p1_id: sketch.lastPoint3D.id, p2_id: newPoint.id, id: sketch.entityIdCounter });
+      sketch.entityIdCounter++;
+    }
+    sketch.lastPoint3D = newPoint;
+  } else if (GeometryType.POINT === type) {
+    sketch.points.push(newPoint);
+    sketch.pointsMap[newPoint.id] = newPoint;
+  } else {
+    console.error('The given Geometry type ' + geometryTypeToString(type) + ' is not yet implemented');
+  }
+
+  return sketch;
+};
+
+export const sketchRemoveEntity = (sketch: SketchType, id: number, type: GeometryType): SketchType => {
+  // TODO properly handle other types when they are supported
+  if (GeometryType.LINE === type) {
+    sketch.lines = sketch.lines.filter((line) => line.id !== id);
+    // also have to delete constraints referencing this line
+    sketch.constraints = sketch.constraints.filter((constraint) => constraint.v[3] !== id && constraint.v[4] !== id);
+  } else if (GeometryType.POINT === type) {
+    sketch.points = sketch.points.filter((point) => point.id !== id);
+    delete sketch.pointsMap[id];
+    // also have to delete constraints referencing this point
+    sketch.constraints = sketch.constraints.filter((constraint) => constraint.v[1] !== id && constraint.v[2] !== id);
+  } else {
+    console.error('The given Geometry type ' + geometryTypeToString(type) + ' is not yet implemented');
+  }
+
+  return sketch;
+};
+
+export const sketchResetLastPoint = (sketch: SketchType): SketchType => {
+  sketch.lastPoint3D = null;
+  return sketch;
+};
+
+export const sketchAddConstraint = (sketch: SketchType, payload: ConstraintType): SketchType => {
+  if (payload.t === SlvsConstraints.SLVS_C_PT_PT_DISTANCE) {
+    // We get the line, but for the backend we need the points
+    const line = sketch.lines.filter((line) => line.id === payload.v[3]);
+    if (line.length >= 1) {
+      const pt1 = sketch.pointsMap[line[0].p1_id];
+      const pt2 = sketch.pointsMap[line[0].p2_id];
+      line[0].length = payload.v[0] as number; // update the length
+      sketch.constraints.push({
+        id: sketch.constraintIdCounter,
+        t: SlvsConstraints.SLVS_C_PT_PT_DISTANCE,
+        v: [payload.v[0], pt1.id, pt2.id, 0, 0],
+      });
+      sketch.constraintIdCounter++;
+    } else {
+      console.warn('Line with id ', payload.v[3], ' could not be found. Cannot add constraint ', payload);
+    }
+  } else {
+    sketch.constraints.push({ ...payload, id: sketch.constraintIdCounter });
+    sketch.constraintIdCounter++;
+  }
+
+  return sketch;
+};
+
+export const sketchUpdateConstraint = (sketch: SketchType, payload: ConstraintType): SketchType => {
+  const index = sketch.constraints.findIndex((constraint) => constraint.id === payload.id);
+  if (index !== -1) {
+    const constraint = sketch.constraints[index];
+    if (constraint.t === SlvsConstraints.SLVS_C_PT_PT_DISTANCE) {
+      const pt1 = sketch.pointsMap[payload.v[1] as number];
+      const pt2 = sketch.pointsMap[payload.v[2] as number];
+      const line = sketch.lines.filter((line) => line.p1_id === pt1.id && line.p2_id === pt2.id);
+      if (line.length >= 1) {
+        line[0].length = payload.v[0] as number; // update the length
+      }
+    }
+    sketch.constraints.splice(index, 1, { ...constraint, ...payload });
+  }
+
+  return sketch;
+};
+
+export const sketchDeleteConstraint = (sketch: SketchType, payload: ConstraintType): SketchType => {
+  sketch.constraints = sketch.constraints.filter((constraint) => constraint.id !== payload.id);
+  if (payload.t === SlvsConstraints.SLVS_C_PT_PT_DISTANCE) {
+    const pt1 = sketch.pointsMap[payload.v[1] as number];
+    const pt2 = sketch.pointsMap[payload.v[2] as number];
+    const line = sketch.lines.filter((line) => line.p1_id === pt1.id && line.p2_id === pt2.id);
+    if (line.length >= 1) {
+      line[0].length = undefined; // update the length
+    }
+  }
+
+  return sketch;
+};
+
+export const sketchUpdateEntities = (sketch: SketchType, entities: SolverEntityType[]): SketchType => {
+  entities.forEach((element: SolverEntityType) => {
+    // Update points and pointsMap of sketch
+    if (element.t === 'point') {
+      // TODO this needs to be adapted when we support more planes other than xy
+      sketch.pointsMap[element.id] = { ...sketch.pointsMap[element.id], x: element.v[0], y: element.v[1] };
+      const pointIndex = sketch.points.findIndex((p) => p.id === element.id);
+      if (pointIndex !== -1) {
+        sketch.points[pointIndex] = { ...sketch.points[pointIndex], x: element.v[0], y: element.v[1] };
+      } else {
+        console.error('Point index was -1. Inconsistent state between state.pointsMap and state.points of this sketch');
+      }
+    }
+    // TODO support other types, e.g. circles and arcs
+  });
+
+  return sketch;
+};
--- src/app/slices/sketchSlice.ts
+++ src/app/slices/sketchSlice.ts
@@ -1,11 +1,22 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../../app/store';
-import { Point3DMapType, Point3DType } from '../types/Point3DType';
+import { Point3DType } from '../types/Point3DType';
 import { Line3DType } from '../types/Line3DType';
-import { ConstraintType, SlvsConstraints } from '../types/Constraints';
+import { ConstraintType } from '../types/Constraints';
 import axios from 'axios';
 import { SolverRequestType, SolverEntityType } from '../types/SolverTypes';
-import { GeometryType, geometryTypeToString } from '../types/EntityType';
+import { GeometryType } from '../types/EntityType';
+import {
+  emptySketch,
+  sketchAddConstraint,
+  sketchAddEntity,
+  sketchDeleteConstraint,
+  sketchRemoveEntity,
+  sketchResetLastPoint,
+  SketchType,
+  sketchUpdateConstraint,
+  sketchUpdateEntities,
+} from './Sketch';
 
 // Define a type for the slice state
 export interface SketchState {
@@ -15,15 +26,7 @@ export interface SketchState {
   lastSolverDof: number;
   lastSolverFailedConstraints: number[];
 
-  entityIdCounter: number;
-  points: Point3DType[];
-  pointsMap: Point3DMapType;
-  lines: Line3DType[];
-  // required for drawing of lines (stores the last point), not needed to persist
-  lastPoint3D: Point3DType | null;
-
-  constraintIdCounter: number;
-  constraints: ConstraintType[];
+  sketch: SketchType;
 }
 
 // Define the initial state using that type
@@ -34,14 +37,7 @@ const initialState: SketchState = {
   lastSolverDof: -1,
   lastSolverFailedConstraints: [],
 
-  entityIdCounter: 1,
-  points: [],
-  pointsMap: {},
-  lines: [],
-  lastPoint3D: null,
-
-  constraintIdCounter: 0,
-  constraints: [],
+  sketch: { ...emptySketch },
 };
 
 export const buildSolverRequestType = (input: {
@@ -88,102 +84,23 @@ export const sketchSlice = createSlice({
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    addEntity: (state, action: PayloadAction<{ p: Point3DType; type: GeometryType }, string>) => {
-      const newPoint = { ...action.payload.p, id: state.entityIdCounter };
-      state.entityIdCounter++;
-
-      // TODO properly handle other types when they are supported
-      if (GeometryType.LINE === action.payload.type) {
-        if (state.lastPoint3D) {
-          if (!(state.lastPoint3D.id in state.pointsMap)) {
-            state.points.push(state.lastPoint3D);
-            state.pointsMap[state.lastPoint3D.id] = state.lastPoint3D;
-          }
-          state.points.push(newPoint);
-          state.pointsMap[newPoint.id] = newPoint;
-          // add the line
-          state.lines.push({ p1_id: state.lastPoint3D.id, p2_id: newPoint.id, id: state.entityIdCounter });
-          state.entityIdCounter++;
-        }
-        state.lastPoint3D = newPoint;
-      } else if (GeometryType.POINT === action.payload.type) {
-        state.points.push(newPoint);
-        state.pointsMap[newPoint.id] = newPoint;
-      } else {
-        console.error(
-          'The given Geometry type ' + geometryTypeToString(action.payload.type) + ' is not yet implemented'
-        );
-      }
+    addEntity: (state: SketchState, action: PayloadAction<{ p: Point3DType; type: GeometryType }, string>) => {
+      sketchAddEntity(state.sketch, action.payload.p, action.payload.type);
     },
-    removeEntity: (state, { payload }) => {
-      // TODO properly handle other types when they are supported
-      if (GeometryType.LINE === payload.type) {
-        state.lines = state.lines.filter((line) => line.id !== payload.id);
-        // also have to delete constraints referencing this line
-        state.constraints = state.constraints.filter(
-          (constraint) => constraint.v[3] !== payload.id && constraint.v[4] !== payload.id
-        );
-      } else if (GeometryType.POINT === payload.type) {
-        state.points = state.points.filter((point) => point.id !== payload.id);
-        delete state.pointsMap[payload.id];
-        // also have to delete constraints referencing this point
-        state.constraints = state.constraints.filter(
-          (constraint) => constraint.v[1] !== payload.id && constraint.v[2] !== payload.id
-        );
-      } else {
-        console.error('The given Geometry type ' + geometryTypeToString(payload.type) + ' is not yet implemented');
-      }
+    removeEntity: (state: SketchState, { payload }) => {
+      sketchRemoveEntity(state.sketch, payload.id, payload.type);
     },
-    resetLastPoint: (state) => {
-      state.lastPoint3D = null;
+    resetLastPoint: (state: SketchState) => {
+      sketchResetLastPoint(state.sketch);
     },
-    addConstraint: (state, { payload }) => {
-      if (payload.t === SlvsConstraints.SLVS_C_PT_PT_DISTANCE) {
-        // We get the line, but for the backend we need the points
-        const line = state.lines.filter((line) => line.id === payload.v[3]);
-        if (line.length >= 1) {
-          const pt1 = state.pointsMap[line[0].p1_id];
-          const pt2 = state.pointsMap[line[0].p2_id];
-          line[0].length = payload.v[0]; // update the length
-          state.constraints.push({
-            id: state.constraintIdCounter,
-            t: SlvsConstraints.SLVS_C_PT_PT_DISTANCE,
-            v: [payload.v[0], pt1.id, pt2.id, 0, 0],
-          });
-          state.constraintIdCounter++;
-        } else {
-          console.warn('Line with id ', payload.v[3], ' could not be found. Cannot add constraint ', payload);
-        }
-      } else {
-        state.constraints.push({ ...payload, id: state.constraintIdCounter });
-        state.constraintIdCounter++;
-      }
+    addConstraint: (state: SketchState, { payload }) => {
+      sketchAddConstraint(state.sketch, payload);
     },
-    updateConstraint: (state, { payload }) => {
-      const index = state.constraints.findIndex((constraint) => constraint.id === payload.id);
-      if (index !== -1) {
-        const constraint = state.constraints[index];
-        if (constraint.t === SlvsConstraints.SLVS_C_PT_PT_DISTANCE) {
-          const pt1 = state.pointsMap[payload.v[1]];
-          const pt2 = state.pointsMap[payload.v[2]];
-          const line = state.lines.filter((line) => line.p1_id === pt1.id && line.p2_id === pt2.id);
-          if (line.length >= 1) {
-            line[0].length = payload.v[0]; // update the length
-          }
-        }
-        state.constraints.splice(index, 1, { ...constraint, ...payload });
-      }
+    updateConstraint: (state: SketchState, { payload }) => {
+      sketchUpdateConstraint(state.sketch, payload);
     },
-    deleteConstraint: (state, { payload }) => {
-      state.constraints = state.constraints.filter((constraint) => constraint.id !== payload.id);
-      if (payload.t === SlvsConstraints.SLVS_C_PT_PT_DISTANCE) {
-        const pt1 = state.pointsMap[payload.v[1]];
-        const pt2 = state.pointsMap[payload.v[2]];
-        const line = state.lines.filter((line) => line.p1_id === pt1.id && line.p2_id === pt2.id);
-        if (line.length >= 1) {
-          line[0].length = undefined; // update the length
-        }
-      }
+    deleteConstraint: (state: SketchState, { payload }) => {
+      sketchDeleteConstraint(state.sketch, payload);
     },
   },
   extraReducers: (builder) => {
@@ -201,24 +118,7 @@ export const sketchSlice = createSlice({
         state.lastSolverDof = action.payload.dof;
         if (0 === action.payload.code) {
           //console.log('received entities ', action.payload.entities);
-          // Update points and pointsMap of state
-          action.payload.entities.forEach((element: SolverEntityType) => {
-            if (element.t === 'point') {
-              // TODO this needs to be adapted when we support more planes other than xy
-              state.pointsMap[element.id].x = element.v[0];
-              state.pointsMap[element.id].y = element.v[1];
-              const pointIndex = state.points.findIndex((p) => p.id === element.id);
-              if (pointIndex !== -1) {
-                state.points[pointIndex].x = element.v[0];
-                state.points[pointIndex].y = element.v[1];
-              } else {
-                console.error(
-                  'Point index was -1. Inconsistent state between state.pointsMap and state.points of this sketch'
-                );
-              }
-            }
-            // TODO support other types, e.g. circles and arcs
-          });
+          sketchUpdateEntities(state.sketch, action.payload.entities);
         } else {
           // Save the action.payload.failed
           state.lastSolverFailedConstraints = action.payload.failed;
@@ -234,12 +134,12 @@ export const sketchSlice = createSlice({
 export const { addEntity, removeEntity, resetLastPoint, addConstraint, updateConstraint, deleteConstraint } =
   sketchSlice.actions;
 
-export const selectPoints = (state: RootState) => state.sketchs.points;
-export const selectPointsMap = (state: RootState) => state.sketchs.pointsMap;
-export const selectLines = (state: RootState) => state.sketchs.lines;
-export const selectLastPoint = (state: RootState) => state.sketchs.lastPoint3D;
+export const selectPoints = (state: RootState) => state.sketchs.sketch.points;
+export const selectPointsMap = (state: RootState) => state.sketchs.sketch.pointsMap;
+export const selectLines = (state: RootState) => state.sketchs.sketch.lines;
+export const selectLastPoint = (state: RootState) => state.sketchs.sketch.lastPoint3D;
 
-export const selectConstraints = (state: RootState) => state.sketchs.constraints;
+export const selectConstraints = (state: RootState) => state.sketchs.sketch.constraints;
 export const selectLastSolverResultCode = (state: RootState) => state.sketchs.lastSolverResultCode;
 export const selectLastDof = (state: RootState) => state.sketchs.lastSolverDof;
 export const selectLastSolverFailedConstraints = (state: RootState) => state.sketchs.lastSolverFailedConstraints;
